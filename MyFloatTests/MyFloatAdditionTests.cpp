5a9393c0a61fc51d716bd14872265beb1c1cc479
Vito Videtta <vvidetta@yahoo.co.uk>
2023-03-12 08:50:01 +0000

    Automate test case generation for Addition tests.

--- MyFloatTests/MyFloatAdditionTests.cpp
+++ MyFloatTests/MyFloatAdditionTests.cpp
@@ -1,12 +1,18 @@
 #include "MyFloat.h"
 #include "gtest/gtest.h"
 #include <iostream>
+#include <ranges>
 
 struct AdditionTestCase
 {
   double first;
   double second;
   double result;
+
+  friend auto operator<=>(
+    AdditionTestCase const& lhs,
+    AdditionTestCase const& rhs
+  ) = default;
 };
 
 auto operator<<(std::ostream& os, AdditionTestCase const& value) -> std::ostream&
@@ -27,13 +33,36 @@ TEST_P(MyFloatAdditionTests, CheckThatAdditionGivesExpectedResult)
   ASSERT_EQ(expectedResult, x + y);
 }
 
-static const auto AdditionTestCases = ::testing::Values(
-  AdditionTestCase{ 0.0, 0.0, 0.0 },
-  AdditionTestCase{ 0.0, 1.0, 1.0 },
-  AdditionTestCase{ 1.0, 0.0, 1.0 },
-  AdditionTestCase{ 1.0, 1.0, 2.0 },
-  AdditionTestCase{ 0.5, 0.5, 1.0 }
-);
+static auto ensureAdditionIsCommutative(std::vector<AdditionTestCase>& testCases) noexcept -> void
+{
+  for (auto it = std::begin(testCases); it != std::end(testCases); ++it)
+  {
+    if (it->first == it->second) {
+      continue;
+    }
+
+    auto reversedCase = AdditionTestCase{ it->second, it->first, it->result };
+    if (std::ranges::find(testCases, reversedCase) != std::ranges::end(testCases)) {
+      continue;
+    }
+
+    it = testCases.insert(it + 1, reversedCase);
+  }
+}
+
+[[nodiscard]] static auto getAdditionTestCases() noexcept
+{
+  auto testCases = std::vector{
+    AdditionTestCase{ 0.0, 0.0, 0.0 },
+    AdditionTestCase{ 0.0, 1.0, 1.0 },
+    AdditionTestCase{ 1.0, 1.0, 2.0 },
+    AdditionTestCase{ 0.5, 0.5, 1.0 }
+  };
+  ensureAdditionIsCommutative(testCases);
+  return testCases;
+}
+
+static const auto AdditionTestCases = ::testing::ValuesIn(getAdditionTestCases());
 
 INSTANTIATE_TEST_SUITE_P(AdditionTestCases, MyFloatAdditionTests, AdditionTestCases);
 
